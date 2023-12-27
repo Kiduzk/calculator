1018c054279ab9ca0c77c09d9b488c642905de37
Kiduzk <70627866+Kiduzk@users.noreply.github.com>
2023-12-26 23:15:11 -0600

    Add display-screen functionality

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+let displayValue = "0";
+
+function updateDisplayValue(){
+    $(".answer").text(displayValue);
+}
 
 // simluation of button click aimation for the button div
 $(".btn").click(function() {
+    displayValue += this.innerHTML; 
+    updateDisplayValue();   
     this.classList.add("clicked");
     setTimeout(() => {
         this.classList.remove("clicked")
     }, 200);
 })
+
+updateDisplayValue();
