9ae5c15dac979684b5f3a94d59c72dced6764707
Kiduzk <70627866+Kiduzk@users.noreply.github.com>
2023-12-27 20:55:21 -0600

    Add eval functionality

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ function updateDisplayValue(){
 }
 
 // simluation of button click aimation for the button div
+// Update: just found out there is eval() funcion to compute an evaluation which I will be using,
+//         this heavily reduced my underestimation of how long this project would take me
+
+$(".equals").click(function() {
+    displayValue = eval(displayValue);
+    updateDisplayValue()
+})
+
 $(".btn").click(function() {
-    displayValue += this.innerHTML; 
-    updateDisplayValue();   
+    if (this.innerHTML === "=") return
+    if (this.innerHTML === "x") displayValue += "*"
+    else displayValue += this.innerHTML; 
+    updateDisplayValue();
+
+    // button click visual effect
     this.classList.add("clicked");
     setTimeout(() => {
         this.classList.remove("clicked")
     }, 200);
 })
+
 
 updateDisplayValue();
